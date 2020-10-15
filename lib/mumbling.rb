a4f28ad0f6dc39498dd05e09a48cc3db1a762459
nax2uk <azlinayeo@gmail.com>
2020-10-15 16:02:51 +0100

    refactored test and added test for three letter string lowercase input

--- lib/mumbling.rb
+++ lib/mumbling.rb
@@ -1,9 +1,11 @@
 class Mumbling
     def mumble_letters(str)
+ 
         return str if str.length == 0
         return str[0].upcase if str.length == 1
         return str[0].upcase << "-" << str[1].upcase << str[1].downcase if str.length == 2
-        return str[0].upcase << "-" << str[1].upcase << str[1].downcase << "-" << str[2] << str[2].downcase << str[2].downcase if str.length == 3
-        
+        return str[0].upcase << "-" << str[1].upcase << str[1].downcase << "-" << str[2].upcase << str[2].downcase << str[2].downcase if str.length == 3
+
+
     end
 end 
--- spec/mumbling_spec.rb
+++ spec/mumbling_spec.rb
@@ -1,11 +1,10 @@
 require "./lib/mumbling"
 
 describe Mumbling do
+    let(:mumbling) {Mumbling.new}
     context "#mumble_letters" do 
         it "given user enters an empty string, it returns an empty string" do
-            # Arrange 
-            mumbling = Mumbling.new
-            # Act
+            # Arrange && Act
             actual_output = mumbling.mumble_letters("")
             expected_output = ""
 
@@ -15,8 +14,6 @@ describe Mumbling do
         context "given user enters an uppercase letter, it returns the uppercase letter" do
             it "given 'A', returns 'A'" do
                 # Arrange 
-                mumbling = Mumbling.new
-                # Act
                 actual_output = mumbling.mumble_letters("A")
                 expected_output = "A"
 
@@ -25,8 +22,6 @@ describe Mumbling do
             end
             it "given 'B', returns 'B'" do
                 # Arrange 
-                mumbling = Mumbling.new
-                # Act
                 actual_output = mumbling.mumble_letters("B")
                 expected_output = "B"
 
@@ -83,9 +78,7 @@ describe Mumbling do
         end
         context "given user enters a string of length two containing uppercase alphabets, it returns the correct string " do
             it "given user enters 'AB', it returns 'A-Bb" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+                 # Arrange && Act
                 actual_output = mumbling.mumble_letters("AB")
                 expected_output = "A-Bb"
 
@@ -93,9 +86,7 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'BC', it returns 'B-Cc" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+               # Arrange && Act
                 actual_output = mumbling.mumble_letters("BC")
                 expected_output = "B-Cc"
 
@@ -103,9 +94,7 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'XF', it returns 'X-Ff" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+              # Arrange && Act
                 actual_output = mumbling.mumble_letters("XF")
                 expected_output = "X-Ff"
 
@@ -115,9 +104,7 @@ describe Mumbling do
         end
         context "given user enters a string of length two containing lowercase alphabets, it returns the correct string " do
             it "given user enters 'bc', it returns 'B-Cc" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+              # Arrange && Act
                 actual_output = mumbling.mumble_letters("bc")
                 expected_output = "B-Cc"
     
@@ -125,9 +112,7 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'st', it returns 'S-Tt" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+               # Arrange && Act
                 actual_output = mumbling.mumble_letters("st")
                 expected_output = "S-Tt"
     
@@ -135,21 +120,17 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'gf', it returns 'G-Ff" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+                # Arrange && Act
                 actual_output = mumbling.mumble_letters("gf")
                 expected_output = "G-Ff"
     
-                # Assert
+                # Arrange && Act
                 expect(actual_output).to eq(expected_output)
             end
         end
         context "given user enters a string of length three containing uppercase alphabets, it returns the correct string " do
             it "given user enters 'ABB, it returns 'A-Bb-Bbb'" do
-                    # Arrange 
-                mumbling = Mumbling.new
-                # Act
+                # Arrange && Act
                 actual_output = mumbling.mumble_letters("ABB")
                 expected_output = "A-Bb-Bbb"
     
@@ -157,9 +138,7 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'ABC', it returns 'A-Bb-Ccc'" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+                # Arrange && Act
                 actual_output = mumbling.mumble_letters("ABC")
                 expected_output = "A-Bb-Ccc"
 
@@ -167,9 +146,7 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
             it "given user enters 'XTR', it returns 'X-Tt-Rrr'" do
-                # Arrange 
-                mumbling = Mumbling.new
-                # Act
+                # Arrange && Act
                 actual_output = mumbling.mumble_letters("XTR")
                 expected_output = "X-Tt-Rrr"
 
@@ -177,6 +154,32 @@ describe Mumbling do
                 expect(actual_output).to eq(expected_output)
             end
      
+        end
+        context "given user enters a string of length three containing lowercase alphabets, it returns the correct string " do
+            it "given user enters 'abc, it returns it returns 'A-Bb-Ccc'" do
+                # Arrange && Act
+                actual_output = mumbling.mumble_letters("abc")
+                expected_output = "A-Bb-Ccc"
+
+                # Assert
+                expect(actual_output).to eq(expected_output)
+            end
+            it "given user enters 'frg, it returns it returns 'F-Rr-Ggg'" do
+                # Arrange && Act
+                actual_output = mumbling.mumble_letters("frg")
+                expected_output = "F-Rr-Ggg"
+
+                # Assert
+                expect(actual_output).to eq(expected_output)
+            end
+            it "given user enters 'ret', it returns it returns 'R-Ee-Ttt'" do
+                # Arrange && Act
+                actual_output = mumbling.mumble_letters("ret")
+                expected_output = "R-Ee-Ttt"
+
+                # Assert
+                expect(actual_output).to eq(expected_output)
+            end
         end
     end
 end
