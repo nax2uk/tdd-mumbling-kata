ca0bf0a9b003fd2ca0b896d9ee7cee7902468839
nax2uk <azlinayeo@gmail.com>
2020-10-15 16:43:15 +0100

    refactored code

--- lib/mumbling.rb
+++ lib/mumbling.rb
@@ -1,25 +1,11 @@
 class Mumbling
     def mumble_letters(str)
-        if str.length == 0
-            return str
-        end 
-        str_input = str[0].upcase
-        if str.length == 1
-            return str_input 
+        
+        return "" if str.length == 0
+        str_output = str[0].upcase
+        for index in 1...str.length
+            str_output << "-" << str[index].upcase << str[index].downcase * index
         end
-        if str.length == 2
-            index = 1
-            str_input << "-" << str[index].upcase << str[index].downcase 
-            return str_input
-        end
-        if str.length == 3
-            index = 1
-            str_input << "-" << str[index].upcase << str[index].downcase 
-            index = 2
-            str_input << "-" << str[2].upcase << str[2].downcase << str[2].downcase 
-            return str_input
-        end
-
-
+        return str_output
     end
 end 
